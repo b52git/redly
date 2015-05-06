947bb431ea09504cddd67f46b383ac0929ce1806
Edward Rich <edwardwrich@gmail.com>
2015-05-06 19:40:42 -0400

    Added a helper method to categories (application controller)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def categories
+    @categories = Category.all if !defined? @categories #if not defined already (!defined?)
+    @categories
+  end
+  helper_method :categories
+  hide_action :categories
+
 end
