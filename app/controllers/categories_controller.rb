ffcb311c10760ff35ea5647aa96f9cc8f14de8c9
Edward Rich <edwardwrich@gmail.com>
2015-05-06 16:42:30 -0400

    Added route for category, added add category link

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,39 @@
+class CategoriesController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      redirect_to posts_path, flash: { notice: 'Thanks for creating a new category.'}
+    else
+      flash.now[:error] = @category.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+private
+
+  def category_params
+    params.require(:category).permit(:name, :title, :description, :sidebar, :submission_text)
+  end
+
+end
