91e514b90263ef2b9aa924dfb5820d52379b4ef5
Edward Rich <edwardwrich@gmail.com>
2015-05-06 13:45:32 -0400

    Added show function to the application.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,4 +17,15 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def destory
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,6 @@ class Post < ActiveRecord::Base
   validates :title, length: { maximum: 500 }, presence: true
   validates :link, presence: true, if: :link?
   validates :body, presence: true, if: :text?
-  enum post_type: [:link, :text]
+  enum post_type: [:link, :text]  #(enum) this converts a string to an integer (:link is 0, :test is 1) the database post_type is 0 or 1
 
 end
