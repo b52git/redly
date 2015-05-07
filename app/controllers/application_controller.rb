9fd9daba03c3989f4c62196fdc0ded8032acec65
Edward Rich <edwardwrich@gmail.com>
2015-05-06 21:09:29 -0400

    Added Devise gem and configured it for authentication.  Numerous steps taken to generate etc.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def categories
     @categories = Category.all if !defined? @categories #if not defined already (!defined?)
@@ -10,5 +10,13 @@ class ApplicationController < ActionController::Base
   end
   helper_method :categories
   hide_action :categories
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:account_update) << :username
+  end
+
 
 end
